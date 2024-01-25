208c36d97f27e95b65f6e36cc375e395d16c3271
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-01-25 16:21:38 +0200

    adding basic styles

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 
 export const MonthStatsTable = () => {
   const [sDate, setsDate] = useState(new Date());
-
+  const previous = '\u003C'
+  const next = '\u003E'
   const findMonthDays = (y, m) => {
     return new Date(y, m + 1, 0).getDate();
   };
@@ -38,13 +39,12 @@ export const MonthStatsTable = () => {
     const m = sDate.getMonth();
     const mDays = findMonthDays(y, m);
     const fDay = findFirstDay(y, m);
-
     const allDays = [];
 
     // For empty cells
-    for (let p = 0; p < fDay; p++) {
-      allDays.push(<div key={`em-${p}`} className="box empty"></div>);
-    }
+    // for (let p = 0; p < fDay; p++) {
+    //   allDays.push(<div key={`em-${p}`} className="box empty"></div>);
+    // }
 
     // Show actual days
     for (let d = 1; d <= mDays; d++) {
@@ -54,7 +54,8 @@ export const MonthStatsTable = () => {
       allDays.push(
         <div
           key={`d-${d}`}
-          className={`box ${isSelected ? 'selected' : ''}`}
+          className={css.day}
+        //   className={`box ${isSelected ? 'selected' : ''}`}
           onClick={() => handleDateClick(date)}
         >
           {d}
@@ -66,29 +67,23 @@ export const MonthStatsTable = () => {
   };
 
   return (
-    <div>
-      <div >
-        {' '}
+    <div className={css['calendar-container']}>
+      <div className={css['calendar-header']}>
         <h2>Month</h2>
         <div className={css.monthPicker}>
-          <button className={css['btn-previous']} onClick={changeToPrevMonth}> d </button>
+          <button className={css['btn-previous']} onClick={changeToPrevMonth}> {previous} </button>
           <h2>
             {sDate.toLocaleString('default', {
               month: 'long',
               year: 'numeric',
             })}
           </h2>
-          <button className={css['btn-next']} onClick={changeToNextMonth}> b </button>
+          <button className={css['btn-next']} onClick={changeToNextMonth}> {next} </button>
         </div>
       </div>
-      <div>{showCalendar()} </div>
+      <div className={css['calendar-table']}>{showCalendar()} </div>
       {sDate && <div>Selected Date: {sDate.toLocaleDateString()}</div>}
     </div>
   );
-  // return(
-  //     <div>
-  //        <h2>month</h2>
 
-  //     </div>
-  // )
 };
