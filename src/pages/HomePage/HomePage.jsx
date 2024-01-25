d7f11fb78f09bbf61186c663ee3ec1418815f26b
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-01-25 19:44:18 +0200

    ready for pull

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { MonthStatsTable } from 'components/MonthStatsTable/MonthStatsTable';
 
 const HomePage = () => {
-  return <div>HomePage
-    <MonthStatsTable>
-
-    </MonthStatsTable>
-  </div>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
