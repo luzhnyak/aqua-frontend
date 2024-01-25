6a81d344b4de92b80825111366d05ae6a9bee03f
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-01-25 15:43:50 +0200

    creating component

---
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -0,0 +1,10 @@
+import css from './MonthStatsTable.module.css'
+
+
+export const MonthStatsTable = () =>{
+    return(
+        <div>
+           <h2>month</h2>
+        </div>
+    )
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { MonthStatsTable } from 'components/MonthStatsTable/MonthStatsTable';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>HomePage
+    <MonthStatsTable>
+
+    </MonthStatsTable>
+  </div>;
 };
 
 export default HomePage;
