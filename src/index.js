7d111085e321f23937963da9260f002af8d4bf86
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-01-26 01:00:40 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from 'redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import './fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-         <BrowserRouter basename="aqua-frontend">
+        <BrowserRouter basename="aqua-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
