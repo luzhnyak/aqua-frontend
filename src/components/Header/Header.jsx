7be716e55d07a013e1ceefbcb1cfb27c3246fb6b
KaterynaBachkalo <kozlovets88@gmail.com>
2024-01-26 02:06:42 +0200

    add UserLogo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import css from './Header.module.css';
 import Logo from './Logo';
 import UserAuth from './UserAuth';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import UserLogo from './UserLogo';
 
 const Header = () => {
+  const autorized = useSelector(selectIsLoggedIn);
+
   return (
     <header>
       <nav className={css.nav}>
         <Logo />
-        <UserAuth />
+        {!autorized ? <UserAuth /> : <UserLogo />}
       </nav>
     </header>
   );
--- src/components/Header/UserAuth.jsx
+++ src/components/Header/UserAuth.jsx
@@ -1,55 +1,16 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import avatar from './Ellipse 1.jpg';
+import React from 'react';
+
 import css from './Header.module.css';
 import { Link } from 'react-router-dom';
-// import { ReactComponent as IconUser } from '../../images/icons/user.svg';
-import { ReactComponent as IconChevron } from '../../images/icons/chevron-double-up.svg';
-import UserLogoModal from './UserLogoModal';
+import { ReactComponent as IconUser } from '../../images/icons/user.svg';
 
 const UserAuth = () => {
-  const autorized = useSelector(selectIsLoggedIn);
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
-  // const user = useSelector(selectUser);
-  // const userName = user.name;
-  // const email = user.email;
-  const userName = 'kate';
-  // const userName = '';
-  const email = 'email@mail.com';
-
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-
   return (
     <>
-      {/* {!autorized ? (
-            <Link to="/signin" className={css.signIn}>
-              <p className={css.signInText}>Sign in</p>
-              <IconUser className={css.IconUser} />
-            </Link>
-          ) : ( */}
-      <Link className={css.dropDownMenu} onClick={toggleMenu}>
-        <p className={css.userNameText}>{userName ? userName : ''}</p>
-        {avatar ? (
-          <img srcSet={avatar} className={css.avatar} alt="userAvatar" />
-        ) : (
-          <div className={css.noAvatar}>
-            <span>
-              {userName
-                ? userName.charAt(0).toUpperCase()
-                : email.charAt(0).toUpperCase()}
-            </span>
-          </div>
-        )}
-        <IconChevron className={css.IconChevron} />
+      <Link to="/signin" className={css.signIn}>
+        <p className={css.signInText}>Sign in</p>
+        <IconUser className={css.IconUser} />
       </Link>
-
-      {isMenuOpen && <UserLogoModal />}
-
-      {/* )} */}
     </>
   );
 };
