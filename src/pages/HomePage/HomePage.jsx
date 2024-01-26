8755e6a9253fb3fc212e11c5347cf80ba7ec7bbb
Ira S <iryna.shevchenko2@gmail.com>
2024-01-27 00:55:05 +0200

    HomePage layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,25 @@
 import WaterRatioPanel from 'components/WaterRatioPanel/WaterRatioPanel';
 import React from 'react';
-import css from './HomePage.module.css'
+import css from './HomePage.module.css';
 import { TodayWaterList } from 'components/TodayWaterList/TodayWaterList';
 import { MonthStatsTable } from 'components/MonthStatsTable/MonthStatsTable';
 
 const HomePage = () => {
-  return <div className={css.container}>
-    HomePage
-    <div></div>
-   <div className={css['container-progress']}>
-   <TodayWaterList/>
-   <MonthStatsTable/>
-    </div>
-<WaterRatioPanel />
-  </div>;
+  return (
+    <main className={css.container}>
+      <section className={css.firstSection}>
+        <div className={css.bottleField}>
+          <span className={css.onlyToCheck}>My daily norma</span>
+        </div>
+
+        <WaterRatioPanel />
+      </section>
+      <div className={css['container-progress']}>
+        <TodayWaterList />
+        <MonthStatsTable />
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
