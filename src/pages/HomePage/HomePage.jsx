b34c3dd5baefb185a74c85be693084f030344202
Ira S <iryna.shevchenko2@gmail.com>
2024-01-26 22:14:01 +0200

    Commented Routes, added hover pointer

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,13 @@
+import WaterRatioPanel from 'components/WaterRatioPanel/WaterRatioPanel';
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div>HomePage</div>
+      {/* <WaterRatioPanel /> */}
+    </>
+  );
 };
 
 export default HomePage;
