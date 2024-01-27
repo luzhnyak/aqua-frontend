941b6ccb5ca674ab4d3741d2e54250c1c106f8c3
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-01-27 14:01:23 +0200

    add: addWater reducer

---
+++ src/redux/waterConsumption/selectors.js
@@ -0,0 +1,5 @@
+export const selectWatersToday = state => state.water.items;
+
+export const selectLoading = state => state.water.loading;
+
+export const selectError = state => state.water.error;
--- src/services/waterApi.js
+++ src/services/waterApi.js
@@ -44,11 +44,6 @@ export const updateUserAvatar = async newAvatar => {
 
 export const updateUserInfo = async formData => {
   const { data } = await axios.patch('/users/update-user', formData);
-  // {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // });
   return data;
 };
 
@@ -58,3 +53,8 @@ export const updateWaterNorma = async newWaterRate => {
 };
 
 //==========================WaterData
+
+export const addWater = async newWater => {
+  const { data } = await axios.post('/water', newWater);
+  return data;
+};
