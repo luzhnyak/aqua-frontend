7fe2aa89ba45e07393c444a4940dcbd313effe60
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-01-28 17:05:43 +0200

    fixing styles

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -1,5 +1,4 @@
 import css from './MonthStatsTable.module.css';
-// import { ReactComponent as IconClose } from '../../images/icons/x-mark-outline.svg';
 import { useState } from 'react';
 import PopUpDay from './PopUpDay';
 
@@ -14,29 +13,6 @@ export const MonthStatsTable = () => {
     return new Date(y, m + 1, 0).getDate();
   };
 
-  // const findFirstDay = (y, m) => {
-  //   return new Date(y, m, 1).getDay();
-  // };
-  //   const showPopup = () => {
-  //     const showPopUp = []
-  // showPopUp.push(
-  // <div>
-  //  <div className={css.popUp}>
-  // {`${sDate.toLocaleDateString('en-US', {
-  //         day: 'numeric',
-  //       })},
-  //        ${sDate.toLocaleDateString('en-US', {
-  //         month: 'long',
-  //       })}`}
-  //       <button className={css['popup-close']} onClick={handleCloseClick}></button>
-  //       <p>Daily norma:</p>
-  //       <p>Fulfillment of the daily norm:</p>
-  //       <p>How many servings of water:</p>
-  //     </div>
-  //     </div>
-  //  )
-  //  return showPopUp
-  //   }
 
   const changeToPrevMonth = () => {
     setsDate(pDate => {
@@ -64,37 +40,17 @@ export const MonthStatsTable = () => {
   };
 
   const showCalendar = () => {
-    // const currDate = new Date();
     const y = sDate.getFullYear();
     const m = sDate.getMonth();
     const mDays = findMonthDays(y, m);
-    // const fDay = findFirstDay(y, m);
     const allDays = [];
-
-    // For empty cells
-    // for (let p = 0; p < fDay; p++) {
-    //   allDays.push(<div key={`em-${p}`} className="box empty"></div>);
-    // }
 
     // Show actual days
     for (let d = 1; d <= mDays; d += 1) {
       const date = new Date(y, m, d);
-      // const isSelected = sDate && date.toDateString() === sDate.toDateString();
 
       allDays.push({ day: d, date: date, value: 100 });
 
-      // allDays.push(
-      //   <div className={css['day-cell']} key={`d-${d}`}>
-      //     <button
-      //       className={css.day}
-      //       //   className={`box ${isSelected ? 'selected' : ''}`}
-      //       onClick={() => handleDateClick(date)}
-      //     >
-      //       {d}
-      //     </button>
-      //     <p className={css.percent}> 100%</p>
-      //   </div>
-      // );
     }
 
     return allDays;
@@ -128,7 +84,6 @@ export const MonthStatsTable = () => {
             <div className={css['day-cell']} key={`d-${item.day}`}>
               <button
                 className={css.day}
-                //   className={`box ${isSelected ? 'selected' : ''}`}
                 onClick={() => handleDateClick(item.day, item.date)}
               >
                 {item.day}
