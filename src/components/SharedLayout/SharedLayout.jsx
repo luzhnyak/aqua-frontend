242d8b823dd83a8fa2a74247c54151294a91bcb4
Ira S <iryna.shevchenko2@gmail.com>
2024-01-28 03:19:58 +0200

    clsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,28 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import { Suspense } from 'react';
+
 import Header from 'components/Header/Header';
 import Loader from 'components/Loader/Loader';
+
 import css from './SharedLayout.module.css';
+import clsx from 'clsx';
 
 export const SharedLayout = () => {
   const location = useLocation();
   const home = location.pathname.includes('/home');
   const welcome = location.pathname.includes('/welcome');
+  const signin = location.pathname.includes('/signin');
+  const signup = location.pathname.includes('/signup');
 
   return (
     <div
-      className={`${css.container} ${home ? css.backgroundHomePage : ''} ${
-        welcome ? css.backgroundWelcomePage : ''
-      }`}
+      className={clsx(
+        css.container,
+        { [css.backgroundHomePage]: home },
+        { [css.backgroundWelcomePage]: welcome },
+        { [css.backgroundSigninPage]: signin },
+        { [css.backgroundSignupPage]: signup }
+      )}
     >
       <Header />
       <Suspense fallback={<Loader />}>
