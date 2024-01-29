6950b2a01b9f631be7041699a4b74eb3d61d3df9
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-01-29 16:16:37 +0200

    add selector for items per day

--- src/redux/waterConsumption/selectors.js
+++ src/redux/waterConsumption/selectors.js
@@ -1,5 +1,7 @@
 export const selectWatersToday = state => state.water.items;
 
+export const selectWatersperDay = state => state.water.itemsPerDay;
+
 export const selectLoading = state => state.water.loading;
 
 export const selectError = state => state.water.error;
