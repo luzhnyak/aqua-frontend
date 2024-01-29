9a4b6f4dd497ad7eef1916107976c1bcab75bf64
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-01-29 15:42:43 +0200

    add private route

--- src/HOCs/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-// import { useSelector } from 'react-redux';
-// import { selectToken } from 'redux/auth/selectors';
-// import { Navigate } from 'react-router-dom';
-
-// export const PublicRoute = ({ children, restricted = false }) => {
-//   const token = useSelector(selectToken);
-//   const shouldRedirect = token && restricted;
-//   return shouldRedirect ? <Navigate to="/welcome" /> : children;
-// };
