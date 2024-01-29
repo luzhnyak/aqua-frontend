5112160a8919874cf7f4e4e8d78cfd7a9cac142f
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-01-30 01:47:45 +0200

    fix upload avatar

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import css from './Setting.module.css';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -17,23 +17,31 @@ const SettingModal = ({ onClose }) => {
   const user = useSelector(selectUser);
   const { name, email, avatarURL } = user;
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    if (file) {
-      dispatch(updateAvatarThunk(file));
-      // Скидання значень інпуту
-      fileInputRef.current.value = null;
-    }
-  };
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   if (file) {
+  //     dispatch(updateAvatarThunk(file));
+  //     // Скидання значень інпуту
+  //     fileInputRef.current.value = null;
+  //   }
+  // };
 
   const handleChange = event => {
     setFile(event.target.files[0]);
   };
 
-  const handleButtonClick = () => {
-    // Викликаємо клік на прихованому інпуті, коли натискана кнопка
-    // fileInputRef.current.click();
-  };
+  useEffect(() => {
+    if (file) {
+      dispatch(updateAvatarThunk(file));
+      // Скидання значень інпуту
+      fileInputRef.current.value = null;
+    }
+  }, [file, dispatch]);
+
+  // const handleButtonClick = () => {
+  //   // Викликаємо клік на прихованому інпуті, коли натискана кнопка
+  //   // fileInputRef.current.click();
+  // };
 
   return (
     <div>
@@ -51,21 +59,23 @@ const SettingModal = ({ onClose }) => {
           </div>
         )}
 
-        <form onSubmit={handleSubmit}>
+        <form>
           <input
             id="inputFile"
             type="file"
             onChange={handleChange}
             className={css.input}
-            // ref={fileInputRef}
+            ref={fileInputRef}
           />
           <label htmlFor="inputFile">
             <IconUploadPhoto className={css.iconUploadPhoto} />
+            Upload a photo
           </label>
 
-          <button className={css.uploadPhoto} onClick={handleButtonClick}>
-            Upload a photo
-          </button>
+          {/* <button
+            className={css.uploadPhoto}
+            onClick={handleButtonClick}
+          ></button> */}
         </form>
       </div>
 
