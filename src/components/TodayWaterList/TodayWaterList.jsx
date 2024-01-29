c1806c4a3acd91ad702dc35d909d183d872eadc3
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-01-29 20:02:00 +0200

    adding modal for add

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -4,10 +4,21 @@ import { ReactComponent as Plus } from '../../images/icons/plus-small.svg';
 import TodayWaterItem from './TodayWaterItem';
 import css from './TodayWaterList.module.css'
 import { selectWatersToday } from '../../redux/waterConsumption/selectors';
+import { useState } from 'react';
+import Modal from 'components/Modal/Modal';
+import AddWaterModal from 'components/AddWaterModal/AddWaterModal';
 
 export const TodayWaterList = () => {
 const entries = useSelector(selectWatersToday)
+const [isOpen, setAddWaterModalOpen] = useState(false);
 
+const openModal = () => {
+  setAddWaterModalOpen(true);
+};
+
+const closeModal = () => {
+  setAddWaterModalOpen(false);
+};
     return(
 <div className={css.entryList}>
     <h2 className={css.title}>Today</h2>
@@ -17,7 +28,12 @@ const entries = useSelector(selectWatersToday)
     )}
     
    </ul>
-   <button className={css['btn-add']}> <Plus className={css.plus}/> Add water</button>
+   <button className={css['btn-add']} onClick={openModal}> <Plus className={css.plus}/> Add water</button>
+   {isOpen && (
+        <Modal title="Add Water" onClose={closeModal}>
+          <AddWaterModal isAddWater={true} />
+        </Modal>
+      )}
 </div>
     )
 }
