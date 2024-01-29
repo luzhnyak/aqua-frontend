1ecd13f1de13afe1833d80357f47836008aec36e
Ira S <iryna.shevchenko2@gmail.com>
2024-01-29 19:07:43 +0200

    AddWaterModal updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,9 @@
-import WaterRatioPanel from 'components/WaterRatioPanel/WaterRatioPanel';
 import React from 'react';
 import css from './HomePage.module.css';
+
 import { TodayWaterList } from 'components/TodayWaterList/TodayWaterList';
 import { MonthStatsTable } from 'components/MonthStatsTable/MonthStatsTable';
+import WaterRatioPanel from 'components/WaterRatioPanel/WaterRatioPanel';
 import DailyNorma from 'components/DailyNorma/DailyNorma';
 
 const HomePage = () => {
