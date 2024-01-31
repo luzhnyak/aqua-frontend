f07ff1933f1eaf128fbfdef5fe292e33195b9091
Ira S <iryna.shevchenko2@gmail.com>
2024-01-31 13:21:13 +0200

    Update AddWaterModal.jsx

--- src/components/AddWaterModal/AddWaterModal.jsx
+++ src/components/AddWaterModal/AddWaterModal.jsx
@@ -47,7 +47,7 @@ const AddWaterModal = ({ isAddWater, isEditWater }) => {
   useEffect(() => {
     const now = getCurrentTime();
     setTime(now);
-  }, [setTime, time]);
+  }, [setTime]);
 
   const generateTimeOptions = () => {
     const options = [];
@@ -67,7 +67,6 @@ const AddWaterModal = ({ isAddWater, isEditWater }) => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log('time', time);
     dispatch(addWaterThunk({ time, waterVolume }));
 
     setCleanStatus({
@@ -90,6 +89,16 @@ const AddWaterModal = ({ isAddWater, isEditWater }) => {
 
   return (
     <>
+      {isEditWater && (
+        <div>
+          <div>
+            <span>Value</span>
+            <span>Time</span>
+          </div>
+          <h2 className={css.headlines}>Correct entered data:</h2>
+        </div>
+      )}
+      {isAddWater && <h2 className={css.headlines}>Choose a value:</h2>}
       <p className={css.paragraphs}>Amount of water:</p>
       <div>
         <button type="button" onClick={minusAmountOfWater}>
@@ -147,13 +156,3 @@ const AddWaterModal = ({ isAddWater, isEditWater }) => {
 };
 
 export default AddWaterModal;
-
-// {isEditWater && (
-//   <div>
-//     <div>
-//       <span>Value</span>
-//       <span>Time</span>
-//     </div>
-//     <h2 className={css.headlines}>Correct entered data:</h2>
-//   </div>
-// )}
