bd1de8b1b33a1e56d773c0c7943b2c4e6bd5cb00
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-02-03 18:36:43 +0200

    delete javascript

--- src/components/Loader/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import css from './Loader.module.css';
-// import { useSelector } from 'react-redux';
-
-const Loader = () => {
-  return <div className={css.loader}></div>;
-};
-
-export default Loader;
--- src/services/handleApiError.js
+++
@@ -1,9 +0,0 @@
-export const handleApiError = error => {
-  if (error.response && error.response.status) {
-    const errorMessage = error.response.data.message || 'Unknown error';
-    const errorCode = error.response.status;
-    return { errorMessage, errorCode };
-  } else {
-    return { errorMessage: 'Server error', errorCode: 500 };
-  }
-};
