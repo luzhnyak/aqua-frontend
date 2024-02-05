d491ca4cea3c606ef6c0b70eeae5e836c5d91115
Ira S <iryna.shevchenko2@gmail.com>
2024-02-05 06:47:54 +0200

    thumb. animation added

--- src/components/WaterRatioPanel/WaterRatioPanel.tsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.tsx
@@ -10,15 +10,16 @@ import { useTranslation } from "react-i18next";
 const WaterRatioPanel = () => {
   const { t } = useTranslation();
 
-  const waterToday = useSelector(selectWatersToday) || 0;
+ const waterToday = useSelector(selectWatersToday) || 0;
   const progressValue = useMemo(() => {
     if (waterToday) {
-      return Number(parseInt(waterToday?.progress));
+      const value = Number(parseInt(waterToday?.progress));
+      return value > 100 ? 100 : value;
     }
     return 0;
   }, [waterToday]);
   const [isOpen, setAddWaterModalOpen] = useState(false);
-  // const [isAddWater, setIsAddWater] = useState(false);
+  const [previousValue, setValue] = useState(progressValue || 0);
 
   const openModal = () => {
     setAddWaterModalOpen(true);
@@ -30,22 +31,21 @@ const WaterRatioPanel = () => {
     document.body.classList.remove("body-scroll-lock");
   };
 
-  // const toggleIsAddWaterState = () => {
-  //   setIsAddWater(!isAddWater);
-  // };
-
   useEffect(() => {
     const rangeThumb = document.getElementById("range-thumb");
     const rangeInput = document.getElementById("range-input");
+    const rangeValue = document.getElementById("range-number");
+    const fifty = document.getElementById("range-thumb");
+    const hundred = document.getElementById("range-input");
 
-    // const thumbPosition = rangeInput.value / rangeInput.max;
     const thumbPosition = progressValue / 100;
 
     const changeThumbPosition = () => {
-      if (rangeThumb && rangeInput) {
+      if (rangeThumb && rangeInput && rangeValue) {
         const space = rangeInput.offsetWidth - rangeThumb.offsetWidth;
         const thumbPositionX = thumbPosition * space;
         rangeThumb.style.left = `${thumbPositionX}px`;
+        rangeValue.style.left = `${thumbPositionX - 3}px`;
       }
     };
 
@@ -58,8 +58,38 @@ const WaterRatioPanel = () => {
         rangeInput.style.background = `linear-gradient(to right, var(--secondary-color-blue-3) 0%, var(--secondary-color-blue-3) ${thumbPositionX}px, var(--secondary-color-blue-2) ${thumbPositionX}px, var(--secondary-color-blue-2) 100%)`;
       }
     };
+
     updateProgressBarColor();
+
   }, [waterToday, progressValue]);
+
+  useEffect(() => {
+    const quickCount = () => {
+      const plusDifference = progressValue - previousValue;
+      const minusDifference = previousValue - progressValue;
+
+      if (plusDifference > 13 || minusDifference > 13) {
+        setValue(progressValue);
+      } else if (plusDifference <= 13 || minusDifference <= 13) {
+        if (progressValue > previousValue) {
+          for (let i = previousValue; i <= progressValue; i++) {
+            setTimeout(() => {
+              setValue(i);
+            }, (i - previousValue) * 100);
+          }
+        } else if (progressValue < previousValue) {
+          for (let i = previousValue; i >= progressValue; i--) {
+            setTimeout(() => {
+              setValue(i);
+            }, (previousValue - i) * 100);
+          }
+        }
+      }
+    };
+
+    quickCount();
+  }, [previousValue, progressValue]);
+
 
   return (
     <div>
@@ -72,13 +102,18 @@ const WaterRatioPanel = () => {
                 <div className={css.rangeSliderLine} id="range-line"></div>
               </div>
 
+           
+              <div className={css.rangeValue} id="range-number">
+                <span className={css.rangeValueNumber}>{previousValue}%</span>
+              </div>
+
               <input
                 type="range"
                 className={css.rangeInput}
                 id="range-input"
                 min="0"
                 max="100"
-                value={progressValue}
+                value={progressValue <= 100 ? progressValue : 100}
                 step="1"
                 readOnly
               />
@@ -87,15 +122,15 @@ const WaterRatioPanel = () => {
           </div>
 
           <div className={css.decorativeLines}>
-            <div className={css.line}></div>
-            <div className={css.line}></div>
+            <div  className={css.line}></div>
+            {/* <div className={css.line}></div> */}
             <div className={css.line}></div>
           </div>
 
           <div className={css.percentages}>
-            <span className={css.number1}>0%</span>
-            <span className={css.number2}>50%</span>
-            <span className={css.number3}>100%</span>
+            <span className={css.number1} id='fifty'>0%</span>
+            {/* <span className={css.number2} id='fifty'>50%</span> */}
+            <span className={css.number3} id='oneHundred'>100%</span>
           </div>
         </div>
         <button type="button" className={css.addButton} onClick={openModal}>
