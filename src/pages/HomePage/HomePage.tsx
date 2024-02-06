d8bdc382b4db8ec85e330d6ddc79e2455d36e15b
Ira S <iryna.shevchenko2@gmail.com>
2024-02-06 19:40:38 +0200

    errors notifications for hope page added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,14 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import css from "./HomePage.module.css";
+
+import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
+
+import { useSelector } from "react-redux";
+import {
+  selectWaterError,
+} from "../../redux/waterConsumption/selectors";
+import { IError } from "../../services/handleApiError";
 
 import { TodayWaterList } from "../../components/TodayWaterList/TodayWaterList";
 import { MonthStatsTable } from "../../components/MonthStatsTable/MonthStatsTable";
@@ -7,7 +16,23 @@ import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
 import DailyNorma from "../../components/DailyNorma/DailyNorma";
 
 const HomePage: FC = () => {
-  // const [isPopUp, setIsPopUp] = useState(false);
+  const { t } = useTranslation();
+  
+  const error: IError | null = useSelector(selectWaterError);
+  
+    useEffect(() => {
+    if (error?.errorCode === 400) {
+      toast.error(`${t("400 Bad request. Invalid data")}`);
+      return;
+    } else if (error?.errorCode === 401) {
+      toast.error(`${t("Not authorized. Please log in!")}`);
+      return;
+    }else if (error?.errorCode === 500) {
+      toast.error(`${t("Server error")}`);
+      return;
+    }
+  }, [error, t]);
+
 
   return (
     <section className={css.section}>
