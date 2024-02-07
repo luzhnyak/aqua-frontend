c401a3b8d632b8d64d7201a0a7dc0fb45315e447
Ira S <iryna.shevchenko2@gmail.com>
2024-02-07 20:39:25 +0200

    progressbar

--- src/components/WaterRatioPanel/WaterRatioPanel.tsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import css from "./WaterRatioPanel.module.css";
 
 import { useSelector } from "react-redux";
